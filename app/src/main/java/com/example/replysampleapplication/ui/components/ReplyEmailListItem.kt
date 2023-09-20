c7f4de1ee1939ab1200f77dadece18b709691615
HamidReza Farahani <h.farahani>
2023-09-20 21:14:15 +0430

    create reply email list item

---
+++ app/src/main/java/com/example/replysampleapplication/ui/components/ReplyEmailListItem.kt
@@ -0,0 +1,108 @@
+package com.example.replysampleapplication.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.semantics.selected
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.replysampleapplication.data.Email
+import com.example.replysampleapplication.data.LocalEmailsDataProvider
+import com.example.replysampleapplication.ui.theme.ReplySampleApplicationTheme
+
+@Composable
+fun ReplyEmailListItem(
+    modifier: Modifier = Modifier,
+    email: Email,
+    isSelected: Boolean = false,
+    navigateToDetail: (Long) -> Unit
+) {
+    Card(
+        modifier = modifier
+            .padding(horizontal = 16.dp, vertical = 4.dp)
+            .semantics { selected = isSelected }
+            .clickable { navigateToDetail(email.id) },
+        colors = CardDefaults.cardColors(
+            containerColor = if (email.isImportant) MaterialTheme.colorScheme.secondaryContainer
+            else MaterialTheme.colorScheme.surfaceVariant
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                ReplyProfileImage(
+                    picture = email.sender.avatar,
+                    description = email.sender.fullName
+                )
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 4.dp)
+                        .weight(1f),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = email.sender.firstName,
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                    Text(
+                        text = email.createdAt,
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier.clip(CircleShape)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "Favorite"
+                    )
+                }
+            }
+            Text(
+                text = email.subject,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(top = 12.dp, bottom = 8.dp)
+            )
+            Text(
+                text = email.body,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun ReplyEmailListItemPreview() {
+    ReplySampleApplicationTheme {
+        ReplyEmailListItem(
+            email = LocalEmailsDataProvider.create()
+        ) {}
+    }
+}
