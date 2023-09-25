49e87d8bd6e3661b95fa7de036b277a2bfacc975
HamidReza Farahani <h.farahani>
2023-09-25 16:17:14 +0330

    finished, using reply app in main activity.

--- app/src/main/java/com/example/replysampleapplication/MainActivity.kt
+++ app/src/main/java/com/example/replysampleapplication/MainActivity.kt
@@ -3,23 +3,40 @@ package com.example.replysampleapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import com.example.replysampleapplication.ui.ReplyApp
+import com.example.replysampleapplication.ui.ReplyHomeViewModel
 import com.example.replysampleapplication.ui.theme.ReplySampleApplicationTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<ReplyHomeViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+
+            val replyUiState by viewModel.uiState.collectAsState()
+
             ReplySampleApplicationTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // TODO: Reply application UI
+                    ReplyApp(
+                        replyHomeUIState = replyUiState,
+                        closeDetailScreen = { viewModel.closeDetailScreen() },
+                        navigateToDetail = {
+                            viewModel.setSelectedEmail(it)
+                        }
+                    )
                 }
             }
         }
